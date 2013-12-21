2b692ee0f10ade8aa0922036b2230c8d06884798
Yuichi Kuroda <pochi.black@gmail.com>
2013-12-22 01:41:46 +0900

    Create index and create

--- capword/controller.go
+++ capword/controller.go
@@ -1,21 +1,54 @@
 package capword
 
 import (
+  "appengine"
+  "appengine/datastore"
   "encoding/json"
   "net/http"
 )
 
 type Word struct {
+  Id   int64
   Name string
 }
 
+type Words struct {
+  Elements []Word
+}
+
 func init() {
-  http.HandleFunc("/words", index)
+  http.HandleFunc("/words/index", index)
+  http.HandleFunc("/words/create", create)
 }
 
 func index(response http.ResponseWriter, request *http.Request) {
   response.Header().Set("Content-Type", "application/json")
+  context := appengine.NewContext(request)
+  key := datastore.NewKey(context, "Words", "default_words", 0, nil)
+  query := datastore.NewQuery("Words").Ancestor(key).Order("Name").Limit(10)
+  words := make([]Word, 0, 10)
+  keys, _ := query.GetAll(context, &words)
+  for i := 0; i < len(words); i++ {
+    words[i].Id = keys[i].IntID()
+  }
+
+  response_words := Words{Elements: words}
+
   encoder := json.NewEncoder(response)
-  word := Word{"happy"}
-  encoder.Encode(word)
+  encoder.Encode(response_words)
+}
+
+func create(response http.ResponseWriter, request *http.Request) {
+  context := appengine.NewContext(request)
+  word := Word{Name: "ruby"}
+  base_key := datastore.NewKey(context, "Words", "default_words", 0, nil)
+  key := datastore.NewIncompleteKey(context, "Words", base_key)
+  datastore_word, error := datastore.Put(context, key, &word)
+  if error != nil {
+    http.Error(response, error.Error(), http.StatusInternalServerError)
+    return
+  }
+  response.Header().Set("Content-Type", "application/json")
+  encoder := json.NewEncoder(response)
+  encoder.Encode(datastore_word)
 }
