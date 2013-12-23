4272a2ead5237e295900204750b82baded07c243
Yuichi Kuroda <pochi.black@gmail.com>
2013-12-24 00:48:26 +0900

    Resource fetch GAE datastore

--- static/javascripts/capword.js
+++ static/javascripts/capword.js
@@ -1,6 +1,29 @@
 'use strict';
 
-var capwordApp = angular.module('capwordApp', []);
+var capwordApp = angular.module('capwordApp', ['capwordService']);
+var capwordService = angular.module('capwordService', ['ngResource']);
 
-capwordApp.controller('capwordController', function($scope) {
+capwordService.factory("WordsManager", function($resource) {
+  var Words = $resource('/words/index', {
+    format: 'json'
+  });
+
+  var WordsManager = {
+    current: [],
+    get: function() {
+      var self = this;
+      Words.get({}, function(words, header) {
+        angular.forEach(words.Elements, function(word, _) {
+          self.current.push(word);
+        });
+      });
+      return this.current;
+    }
+  };
+
+  return WordsManager;
+});
+
+capwordApp.controller('capwordController', function($scope, WordsManager) {
+  $scope.words = WordsManager.get();
 });
